5b9a154384e513caa8542dc29b0a26fc1de3c5f5
felix <lifeng@shzdsoft.com>
2020-12-10 18:14:11 +0800

    增加yaml配置读取

---
+++ include/core/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+
+class CConfig
+{
+private:
+    /* data */
+public:
+    CConfig();
+    ~CConfig();
+};
---
+++ shell/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "core/config.h"
+
+int main() {
+   
+    std::cout << "Jupiter Hub Server starting!" << std::endl;
+
+    CConfig config;
+
+
+    getchar();
+    return 0;
+}
